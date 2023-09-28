c259d3f5d22e43188261f1467e50bc386b9a934a
Zaldy Jr Pagaduan <zaldypagaduanjr@gmail.com>
2023-09-29 00:42:09 +0800

    added camera drone behavior

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ElementRef } from "@angular/core";
-import { Viewer, Cesium3DTileset, Cartesian3, Math } from "cesium";
+import {
+  Viewer,
+  Cesium3DTileset,
+  Cartesian3,
+  Math,
+  HeadingPitchRange,
+} from "cesium";
 import { environment } from "src/environments/environment";
 
 @Component({
@@ -11,6 +17,8 @@ export class MapComponent implements OnInit {
   constructor(private el: ElementRef) {}
 
   async ngOnInit(): Promise<void> {
+    let latitude = 7.072156524885305;
+    let longitude = 125.61208610717645;
     const viewer = new Viewer(this.el.nativeElement, {
       globe: false,
       baseLayerPicker: false,
@@ -27,15 +35,27 @@ export class MapComponent implements OnInit {
     );
     viewer.scene.primitives.add(tileset);
     viewer.camera.setView({
-      destination: Cartesian3.fromDegrees(
-        125.61208610717645,
-        7.072156524885305,
-        144.0
-      ),
+      destination: Cartesian3.fromDegrees(longitude, latitude, 144.0),
       orientation: {
         heading: Math.toRadians(10),
         pitch: Math.toRadians(-10),
       },
+    });
+
+    //helicopter
+    let heading = 0; //or any starting angle in radians
+    let rotation = -1; //counter-clockwise; +1 would be clockwise
+    let centre = Cartesian3.fromDegrees(longitude, latitude);
+    let elevation = 333; // 100 meters
+    let pitch = -0.7854; //looking down at 45 degrees
+    const SMOOTHNESS = 600;
+
+    viewer.clock.onTick.addEventListener(() => {
+      heading += (rotation * Math.PI) / SMOOTHNESS;
+      viewer.camera.lookAt(
+        centre,
+        new HeadingPitchRange(heading, pitch, elevation)
+      );
     });
   }
 }
