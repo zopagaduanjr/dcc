a0950dc6456938dfcdade19daf1eb4f194cebd0b
Zaldy Jr Pagaduan <zaldypagaduanjr@gmail.com>
2023-10-01 02:30:14 +0800

    added overview coords

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DataService } from "../services/data.service";
 
 @Component({
-  selector: 'app-info-box',
-  templateUrl: './info-box.component.html',
-  styleUrls: ['./info-box.component.css']
+  selector: "app-info-box",
+  templateUrl: "./info-box.component.html",
+  styleUrls: ["./info-box.component.css"],
 })
 export class InfoBoxComponent {
+  dataService?: DataService;
+  constructor(_dataService: DataService) {
+    this.dataService = _dataService;
+  }
 
+  onSelect(): void {
+    console.log(
+      "AMDG cartographic",
+      this.dataService?.viewer?.camera.positionCartographic
+    );
+    console.log("AMDG positionWC", this.dataService?.viewer?.camera.positionWC);
+    console.log(
+      "Heading Pitch Roll",
+      `(${this.dataService?.viewer?.scene?.camera?.heading},${this.dataService?.viewer?.scene?.camera?.pitch}, ${this.dataService?.viewer?.scene?.camera?.roll})`
+    );
+  }
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,6 +6,7 @@ import {
   Cartesian3,
   Math,
   HeadingPitchRange,
+  HeadingPitchRoll,
   RequestScheduler,
   LabelStyle,
   VerticalOrigin,
@@ -15,6 +16,7 @@ import {
 import { environment } from "src/environments/environment";
 import { coffeeshops } from "../coffeeshops";
 import { CoffeeShop } from "../coffeeshop";
+import { DataService } from "../services/data.service";
 
 @Component({
   selector: "app-map",
@@ -36,13 +38,26 @@ export class MapComponent implements OnInit {
     animation: false,
     requestRenderMode: true,
   };
-  viewer: Viewer | undefined;
+  viewer?: Viewer;
+  dataService?: DataService;
+  overViewCartesian3: Cartesian3 = new Cartesian3(
+    -3687900.922436941,
+    5150484.382833073,
+    772354.1123908913
+  );
+  overViewOrientation: HeadingPitchRoll = new HeadingPitchRoll(
+    0.20033943352436978,
+    -0.3990495255836124,
+    0.000009647921471511722
+  );
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef, _dataService: DataService) {
+    this.dataService = _dataService;
+  }
 
   async ngOnInit(): Promise<void> {
     await this.setupViewer();
-    // this.rotateCamera();
+    this.rotateCamera();
     coffeeshops.forEach((e) => this.createMarker(e));
   }
 
@@ -72,21 +87,21 @@ export class MapComponent implements OnInit {
     }
 
     this.viewer.camera.setView({
-      destination: Cartesian3.fromDegrees(
-        this.coffeeShop.geometry.location.lng,
-        this.coffeeShop.geometry.location.lat,
-        this.coffeeShop.elevation + this.elevationViewerOffset
-      ),
-      orientation: {
-        heading: 0,
-        pitch: -Math.PI / 2,
-      },
+      destination: this.overViewCartesian3,
+      orientation: this.overViewOrientation,
     });
+    if (this.dataService != null) {
+      this.dataService.viewer = this.viewer;
+    }
   }
 
   rotateCamera(): void {
     //TODO: stop rotating if user taps on scene. start rotating if user idles
-    this.pointCameraAt();
+    // this.pointCameraAt();
+    this.viewer?.camera.lookAt(
+      this.overViewCartesian3,
+      new HeadingPitchRange(0, -0.3990495255836124, 50)
+    );
     this.viewer?.clock.onTick.addEventListener(() => {
       this.viewer?.camera.rotate(Cartesian3.UNIT_Z);
     });
