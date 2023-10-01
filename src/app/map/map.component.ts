cb0b92a488c54e54460ef457016390cf2ee86f36
Zaldy Jr Pagaduan <zaldypagaduanjr@gmail.com>
2023-10-01 09:24:32 +0800

    added interpol test

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,18 +1,5 @@
 import { Component, OnInit, ElementRef } from "@angular/core";
-import {
-  Viewer,
-  Cesium3DTileset,
-  Cartesian2,
-  Cartesian3,
-  Math,
-  HeadingPitchRange,
-  HeadingPitchRoll,
-  RequestScheduler,
-  LabelStyle,
-  VerticalOrigin,
-  DistanceDisplayCondition,
-  Ion,
-} from "cesium";
+import * as Cesium from "cesium";
 import { environment } from "src/environments/environment";
 import { coffeeshops } from "../coffeeshops";
 import { CoffeeShop } from "../coffeeshop";
@@ -27,7 +14,7 @@ export class MapComponent implements OnInit {
   coffeeShop: CoffeeShop = coffeeshops[0];
   elevationMarkerOffset: number = 74;
   elevationViewerOffset: number = 150;
-  viewerOptions: Viewer.ConstructorOptions = {
+  viewerOptions: Cesium.Viewer.ConstructorOptions = {
     globe: false,
     baseLayerPicker: false,
     homeButton: false,
@@ -37,15 +24,16 @@ export class MapComponent implements OnInit {
     fullscreenButton: false,
     animation: false,
     requestRenderMode: true,
+    shouldAnimate: true,
   };
-  viewer?: Viewer;
+  viewer?: Cesium.Viewer;
   dataService?: DataService;
-  overViewCartesian3: Cartesian3 = new Cartesian3(
+  overViewCartesian3: Cesium.Cartesian3 = new Cesium.Cartesian3(
     -3687900.922436941,
     5150484.382833073,
     772354.1123908913
   );
-  overViewOrientation: HeadingPitchRoll = new HeadingPitchRoll(
+  overViewOrientation: Cesium.HeadingPitchRoll = new Cesium.HeadingPitchRoll(
     0.20033943352436978,
     -0.3990495255836124,
     0.000009647921471511722
@@ -57,17 +45,21 @@ export class MapComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     await this.setupViewer();
-    this.rotateCamera();
+    // this.rotateCamera();
     coffeeshops.forEach((e) => this.createMarker(e));
+    this.tryInterpol();
   }
 
   async setupViewer(): Promise<void> {
-    RequestScheduler.requestsByServer = { "tile.googleapis.com:443": 18 };
+    Cesium.RequestScheduler.requestsByServer = {
+      "tile.googleapis.com:443": 18,
+    };
     var tileset = null;
     try {
-      tileset = await Cesium3DTileset.fromUrl(
-        `https://tile.googleapis.com/v1/3dtiles/root.json?key=${environment.googleMap.mapTiles}`
-      );
+      // tileset = await Cesium3DTileset.fromUrl(
+      //   `https://tile.googleapis.com/v1/3dtiles/root.json?key=${environment.googleMap.mapTiles}`
+      // );
+      throw new Error("dont load maptiles");
     } catch (e) {
       this.viewerOptions = {
         baseLayerPicker: false,
@@ -77,11 +69,12 @@ export class MapComponent implements OnInit {
         timeline: false,
         fullscreenButton: false,
         animation: false,
+        shouldAnimate: true,
       };
-      Ion.defaultAccessToken = environment.cesiumToken;
+      Cesium.Ion.defaultAccessToken = environment.cesiumToken;
       this.elevationMarkerOffset = 0;
     }
-    this.viewer = new Viewer(this.el.nativeElement, this.viewerOptions);
+    this.viewer = new Cesium.Viewer(this.el.nativeElement, this.viewerOptions);
     if (tileset != null) {
       this.viewer.scene.primitives.add(tileset);
     }
@@ -100,29 +93,29 @@ export class MapComponent implements OnInit {
     // this.pointCameraAt();
     this.viewer?.camera.lookAt(
       this.overViewCartesian3,
-      new HeadingPitchRange(0, -0.3990495255836124, 50)
+      new Cesium.HeadingPitchRange(0, -0.3990495255836124, 50)
     );
     this.viewer?.clock.onTick.addEventListener(() => {
-      this.viewer?.camera.rotate(Cartesian3.UNIT_Z);
+      this.viewer?.camera.rotate(Cesium.Cartesian3.UNIT_Z);
     });
   }
 
   pointCameraAt(): void {
     //TODO: update angle of rotation cause coffeeshops are small
     const distance =
-      Cartesian3.distance(
-        Cartesian3.fromDegrees(
+      Cesium.Cartesian3.distance(
+        Cesium.Cartesian3.fromDegrees(
           this.coffeeShop.geometry.viewport.southwest.lng,
           this.coffeeShop.geometry.viewport.southwest.lat,
           this.coffeeShop.elevation
         ),
-        Cartesian3.fromDegrees(
+        Cesium.Cartesian3.fromDegrees(
           this.coffeeShop.geometry.viewport.northeast.lng,
           this.coffeeShop.geometry.viewport.northeast.lat,
           this.coffeeShop.elevation
         )
       ) / 2;
-    const target = Cartesian3.fromDegrees(
+    const target = Cesium.Cartesian3.fromDegrees(
       this.coffeeShop.geometry.location.lng,
       this.coffeeShop.geometry.location.lat,
       this.coffeeShop.elevation
@@ -131,7 +124,7 @@ export class MapComponent implements OnInit {
     const heading = 0;
     this.viewer?.camera.lookAt(
       target,
-      new HeadingPitchRange(heading, pitch, distance)
+      new Cesium.HeadingPitchRange(heading, pitch, distance)
     );
   }
 
@@ -139,7 +132,7 @@ export class MapComponent implements OnInit {
     //problem i think is that marker gets lost in the terrains, hence can't clamp
     this.viewer?.entities.add({
       name: shop.name,
-      position: Cartesian3.fromDegrees(
+      position: Cesium.Cartesian3.fromDegrees(
         shop.geometry.location.lng,
         shop.geometry.location.lat,
         shop.elevation + this.elevationMarkerOffset
@@ -147,18 +140,113 @@ export class MapComponent implements OnInit {
 
       label: {
         text: shop.name,
-        style: LabelStyle.FILL_AND_OUTLINE,
+        style: Cesium.LabelStyle.FILL_AND_OUTLINE,
         outlineWidth: 2,
-        verticalOrigin: VerticalOrigin.BOTTOM,
-        pixelOffset: new Cartesian2(0, -80),
-        distanceDisplayCondition: new DistanceDisplayCondition(0, 1000),
+        verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+        pixelOffset: new Cesium.Cartesian2(0, -80),
+        distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 1000),
       },
 
       billboard: {
         image: shop.logo,
         width: 128,
         height: 64,
-        verticalOrigin: VerticalOrigin.BOTTOM,
+        verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+      },
+    });
+  }
+
+  setCircle(): Cesium.SampledPositionProperty {
+    const start = Cesium.JulianDate.fromDate(new Date(2015, 2, 25, 16));
+
+    var lat = Cesium.Math.toDegrees(0.12348093458854793);
+    var lon = Cesium.Math.toDegrees(2.192388248145831);
+    var radius = 0.04;
+    const property = new Cesium.SampledPositionProperty();
+    for (let i = 0; i <= 360; i += 45) {
+      const radians = Cesium.Math.toRadians(i);
+      const time = Cesium.JulianDate.addSeconds(
+        start,
+        i,
+        new Cesium.JulianDate()
+      );
+      const position = Cesium.Cartesian3.fromDegrees(
+        lon + radius * 1.5 * Math.cos(radians),
+        lat + radius * Math.sin(radians),
+        Cesium.Math.nextRandomNumber() * 500 + 1750
+      );
+      property.addSample(time, position);
+
+      //Also create a point for each sample we generate.
+      this.viewer?.entities.add({
+        position: position,
+        point: {
+          pixelSize: 8,
+          color: Cesium.Color.TRANSPARENT,
+          outlineColor: Cesium.Color.YELLOW,
+          outlineWidth: 3,
+        },
+      });
+    }
+    return property;
+  }
+
+  setViewerClock(): void {
+    const start = Cesium.JulianDate.fromDate(new Date(2015, 2, 25, 16));
+    const stop = Cesium.JulianDate.addSeconds(
+      start,
+      360,
+      new Cesium.JulianDate()
+    );
+    if (this.viewer != null) {
+      this.viewer.clock.startTime = start.clone();
+      this.viewer.clock.stopTime = stop.clone();
+      this.viewer.clock.currentTime = start.clone();
+      this.viewer.clock.clockRange = Cesium.ClockRange.LOOP_STOP; //Loop at the end
+      this.viewer.clock.multiplier = 10;
+    }
+  }
+
+  tryInterpol(): void {
+    this.setViewerClock();
+    const start = Cesium.JulianDate.fromDate(new Date(2015, 2, 25, 16));
+    const stop = Cesium.JulianDate.addSeconds(
+      start,
+      360,
+      new Cesium.JulianDate()
+    );
+    const position = this.setCircle();
+    const entity = this.viewer?.entities.add({
+      //Set the entity availability to the same interval as the simulation time.
+      availability: new Cesium.TimeIntervalCollection([
+        new Cesium.TimeInterval({
+          start: start,
+          stop: stop,
+        }),
+      ]),
+
+      //Use our computed positions
+      position: position,
+
+      //Automatically compute orientation based on position movement.
+      orientation: new Cesium.VelocityOrientationProperty(position),
+
+      //Load the Cesium plane model to represent the entity
+      point: {
+        pixelSize: 50,
+        color: Cesium.Color.RED,
+        outlineColor: Cesium.Color.WHITE,
+        outlineWidth: 2,
+      },
+
+      //Show the path as a pink line sampled in 1 second increments.
+      path: {
+        resolution: 1,
+        material: new Cesium.PolylineGlowMaterialProperty({
+          glowPower: 0.1,
+          color: Cesium.Color.YELLOW,
+        }),
+        width: 30,
       },
     });
   }
